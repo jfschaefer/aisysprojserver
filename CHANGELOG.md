be515764760cf286952c4d60ea2f703e5620a13f
jfschaefer <jan.frederik.schaefer@fau.de>
2025-07-31 12:27:13 +0200

    bugfix: do not count old runs for run_limit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 0.1.4 (unreleased)
 * bugfix: proper fix for error when loading env's by group
 * bugfix: extract identifier when loading subgroups
+* bugfix: `run_limit` in client also counted old (and possibly abandoned) runs
 * enhancement: client logs abandoned runs
 
 ## 0.1.3 (released on 2024-12-28)
--- aisysprojserver_clienttools/client.py
+++ aisysprojserver_clienttools/client.py
@@ -110,7 +110,9 @@ class _RunTracker:
                     self.old_runs.remove(rq['run'])
 
         ongoing_runs = set(response['active_runs'])
-        self.number_of_new_runs_finished += len(self.ongoing_runs - ongoing_runs)
+        self.number_of_new_runs_finished += len(
+                self.ongoing_runs - self.old_runs - ongoing_runs
+        )
         self.ongoing_runs = ongoing_runs
 
 
